f6c1792d907c55586c2ce3efd3aabff6c4a973c7
gtulipan <gtulipan@gmail.com>
2024-10-23 12:48:55 +0200

    add controller test toProductController

---
+++ productservice/src/test/java/unit/com/onlineshop/productservice/controller/ProductControllerTest.java
@@ -0,0 +1,26 @@
+package com.onlineshop.productservice.controller;
+
+import com.onlineshop.productservice.model.ProductDto;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ProductControllerTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    //@Test //FIXME
+    void getAllProductsShouldReturnListOfProducts() {
+        webTestClient.get().uri("/v1/products")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(ProductDto.class)
+                .value(products -> assertThat(products).isNotEmpty());
+    }
+}
+
