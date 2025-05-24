4208061ce63c8c7abbad8d1c1041c3dd56156014
gtulipan <gtulipan@gmail.com>
2025-05-24 20:04:02 +0200

    feat:locale-bundles:update security config

--- locale-bundles/src/main/java/com/onlinestore/locale_bundles/config/SecurityConfig.java
+++ locale-bundles/src/main/java/com/onlinestore/locale_bundles/config/SecurityConfig.java
@@ -1,17 +1,24 @@
 package com.onlinestore.locale_bundles.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+
+    private final CorsConfig corsConfig;
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http){
         http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()))
                 .authorizeExchange(exchanges -> exchanges
                         .anyExchange().permitAll()
                 );
